8cd05f92fe4c35bd6e8a86f325e07baeb9eb145f
Nick Eubank <nick@nickeubank.com>
2023-02-01 11:48:19 -0500

    add p-values stub

---
+++ _build/jupyter_execute/40_advanced_topics/10_pvalues_and_decision_making.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # P-Values and Decisions Making
+
+# P-values is $P(\text{Observed Data} | H_0 \text{is true})$, NOT the probability that $H_0 \text{is true}$.
+
+# 
